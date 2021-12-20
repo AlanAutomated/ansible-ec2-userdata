caaf64df5c3e948da040161c8b4da0257a55142d
Alan Haynes <alan@nre.dev>
2021-12-20 09:58:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Ansible EC2 UserData modules
-Local AWS EC2 UserData modules to supplement the official amazon.aws.ec2_instance module
+Local AWS EC2 UserData modules to supplement the official amazon.aws.ec2_instance module. 
+
+## Example Usage
+Create a `library` folder within your ansible project and copy the modules to the folder.
+
+````
+- name: Update EC2 UserData attribute value
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+    user_data: 'foo'
+
+- name: Update EC2 UserData attribute value
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+  register: result
+
+- name: Validate the result
+  debug:
+    var: result
+````
---
+++ ec2_userdata.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ec2_userdata
+
+short_description: Update an EC2 UserData attribute
+
+version_added: "1.0.0"
+
+description: Updates the UserData attribute value of a stopped EC2 instance.
+
+options:
+    aws_access_key:
+        description: If not set then the value of the AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY or EC2_ACCESS_KEY environment variable is used.
+        required: false
+        type: str
+    aws_secret_key:
+        description: If not set then the value of the AWS_SECRET_ACCESS_KEY, AWS_SECRET_KEY, or EC2_SECRET_KEY environment variable is used.
+        required: false
+        type: str
+    instance_id:
+        description: The ID of the EC2 instance.
+        required: true
+        type: str
+    region:
+        description: The region of the EC2 instance.
+        required: true
+        type: str
+    user_data:
+        description: The new UserData attribute value.
+        required: true
+        type: str
+
+author:
+    - Alan Haynes (@alanautomated)
+"""
+
+EXAMPLES = r"""
+- name: Update EC2 UserData attribute value
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+"""
+
+RETURN = r"""
+None
+"""
+
+import os
+
+import boto3
+import botocore
+from ansible.module_utils.basic import AnsibleModule
+from botocore.exceptions import ClientError
+
+
+def run_module():
+    module_args = dict(
+        aws_access_key=dict(type="str", required=False, no_log=True),
+        aws_secret_key=dict(type="str", required=False, no_log=True),
+        instance_id=dict(type="str", required=True),
+        region=dict(type="str", required=True),
+        user_data=dict(type="str", required=True),
+    )
+
+    result = dict(changed=False)
+
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+
+    access_key = module.params.get("aws_access_key")
+    secret_key = module.params.get("aws_secret_key")
+    instance_id = module.params.get("instance_id")
+    region = module.params.get("region")
+    user_data = module.params.get("user_data")
+
+    # Use environment variables if module arguments not passed
+    if not access_key:
+        if os.environ.get("AWS_ACCESS_KEY_ID"):
+            access_key = os.environ["AWS_ACCESS_KEY_ID"]
+        elif os.environ.get("AWS_ACCESS_KEY"):
+            access_key = os.environ["AWS_ACCESS_KEY"]
+        elif os.environ.get("EC2_ACCESS_KEY"):
+            access_key = os.environ["EC2_ACCESS_KEY"]
+        else:
+            module.fail_json(
+                msg="Parameter aws_access_key or equivalent environment variable required."
+            )
+
+    if not secret_key:
+        if os.environ.get("AWS_SECRET_ACCESS_KEY"):
+            secret_key = os.environ["AWS_SECRET_ACCESS_KEY"]
+        elif os.environ.get("AWS_SECRET_KEY"):
+            secret_key = os.environ["AWS_SECRET_KEY"]
+        elif os.environ.get("EC2_SECRET_KEY"):
+            secret_key = os.environ["EC2_SECRET_KEY"]
+        else:
+            module.fail_json(
+                msg="Parameter aws_secret_key or equivalent environment variable required."
+            )
+
+    try:
+        client = boto3.client(
+            "ec2",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region,
+        )
+
+        response = client.modify_instance_attribute(
+            InstanceId=instance_id, UserData={"Value": user_data}
+        )
+    except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+        module.fail_json(msg=str(e))
+
+    try:
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            result["changed"] = True
+    except KeyError:
+        pass
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
+Verifying
