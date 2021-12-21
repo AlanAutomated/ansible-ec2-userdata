199c1278f6280a60efddec6d494285b89b9b43c0
Alan Haynes <alan@nre.dev>
2021-12-21 13:24:04 -0500

    Added idempotency and check mode. Deprecated ec2_userdata_info.

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
-# Ansible EC2 UserData modules
+# Ansible EC2 UserData module
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
-Local AWS EC2 UserData modules to supplement the official amazon.aws.ec2_instance module. 
+An idempotent AWS EC2 UserData module to supplement the official amazon.aws.ec2_instance module. 
 
 ## Example Usage
-Create a `library` folder within your ansible project and copy the modules to the folder.
+Create a `library` folder within your ansible project and copy the module to the folder.
 
 ````
-- name: Update EC2 UserData attribute value of a stopped instance
+- name: Update EC2 UserData attribute value to foo
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+    user_data: foo
+
+- name: Update EC2 UserData attribute value to null
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+
+- name: Compare the current and new UserData attribute values without updating the EC2 instance
   ec2_userdata_info:
     instance_id: i-076XXXX
     region: us-east-1
     user_data: 'foo'
-
-- name: Update EC2 UserData attribute value
-  ec2_userdata_info:
-    instance_id: i-076XXXX
-    region: us-east-1
-  register: result
-
-- name: Validate the result
-  debug:
-    var: result
+  check_mode: on
 ````
--- ec2_userdata.py
+++ ec2_userdata.py
@@ -33,7 +33,7 @@ options:
         type: str
     user_data:
         description: The new UserData attribute value.
-        required: true
+        required: false
         type: str
 
 author:
@@ -41,17 +41,38 @@ author:
 """
 
 EXAMPLES = r"""
-- name: Update EC2 UserData attribute value
+- name: Update EC2 UserData attribute value to foo
   ec2_userdata_info:
     instance_id: i-076XXXX
     region: us-east-1
+    user_data: foo
+- name: Update EC2 UserData attribute value to null
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+- name: Compare the current and new UserData attribute values without updating the EC2 instance
+  ec2_userdata_info:
+    instance_id: i-076XXXX
+    region: us-east-1
+    user_data: 'foo'
+  check_mode: on
 """
 
 RETURN = r"""
-None
+current_user_data:
+    description: The current UserData attribute value associated with the EC2 instance
+    type: str
+    returned: always
+    sample: foo
+new_user_data:
+    description: The new UserData attribute value to be associated with the EC2 instance
+    type: str
+    returned: always
+    sample: bar    
 """
 
 import os
+import base64
 
 import boto3
 import botocore
@@ -65,20 +86,26 @@ def run_module():
         aws_secret_key=dict(type="str", required=False, no_log=True),
         instance_id=dict(type="str", required=True),
         region=dict(type="str", required=True),
-        user_data=dict(type="str", required=True),
+        user_data=dict(type="str", required=False),
     )
 
     result = dict(changed=False)
 
-    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     access_key = module.params.get("aws_access_key")
     secret_key = module.params.get("aws_secret_key")
     instance_id = module.params.get("instance_id")
     region = module.params.get("region")
-    user_data = module.params.get("user_data")
+    new_user_data = module.params.get("user_data")
 
-    # Use environment variables if module arguments not passed
+    # If user_data is omitted, change from None for encoding.
+    if not new_user_data:
+        new_user_data = ""
+
+    result["new_user_data"] = new_user_data
+
+    # Use environment variables if module arguments not passed.
     if not access_key:
         if os.environ.get("AWS_ACCESS_KEY_ID"):
             access_key = os.environ["AWS_ACCESS_KEY_ID"]
@@ -103,25 +130,55 @@ def run_module():
                 msg="Parameter aws_secret_key or equivalent environment variable required."
             )
 
-    try:
-        client = boto3.client(
-            "ec2",
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            region_name=region,
-        )
+    client = boto3.client(
+        "ec2",
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        region_name=region,
+    )
 
-        response = client.modify_instance_attribute(
-            InstanceId=instance_id, UserData={"Value": user_data}
+    current_user_data_bytes = ""
+
+    try:
+        response = client.describe_instance_attribute(
+            Attribute="userData", InstanceId=instance_id
         )
     except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
         module.fail_json(msg=str(e))
 
     try:
-        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-            result["changed"] = True
+        if (
+            response["ResponseMetadata"]["HTTPStatusCode"] == 200
+            and response["UserData"]["Value"]
+        ):
+            current_user_data_bytes = response["UserData"]["Value"]
     except KeyError:
         pass
+
+    result["current_user_data"] = base64.b64decode(current_user_data_bytes)
+
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # Encode the new user_data string for comparison to enable idempotency.
+    new_user_data_bytes = base64.b64encode(str(new_user_data).encode()).decode("ascii")
+
+    if current_user_data_bytes != new_user_data_bytes:
+        try:
+            response = client.modify_instance_attribute(
+                InstanceId=instance_id, UserData={"Value": new_user_data}
+            )
+        except (
+            botocore.exceptions.BotoCoreError,
+            botocore.exceptions.ClientError,
+        ) as e:
+            module.fail_json(msg=str(e))
+
+        try:
+            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                result["changed"] = True
+        except KeyError:
+            pass
 
     module.exit_json(**result)
 
